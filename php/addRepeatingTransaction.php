833cbd4d55220c614722f2b32e58e02b61c59c3f
deliodotexe <lionardo.wernli@gmail.com>
2024-04-06 18:54:50 +0200

    refactored html

--- php/addRepeatingTransaction.php
+++ php/addRepeatingTransaction.php
@@ -3,13 +3,14 @@ require 'db.php';
 
 $input = (array) json_decode(file_get_contents('php://input'), TRUE);
 
-$sql = "INSERT INTO repeatingtransactions (Name, ValidFrom, ValidUntil, Amount) VALUES (:name, :validFrom, :validUntil, :amount)";
+$sql = "INSERT INTO repeatingtransactions (Name, ValidFrom, ValidUntil, Amount, transactionInterval) VALUES (:name, :validFrom, :validUntil, :amount, :interval)";
 $stmt = $pdo->prepare($sql);
 $stmt->execute([
     'mame' => $input['name'],
     'validFrom' => $input['validFrom'],
     'validUntil' => $input['validUntil'],
-    'amount' => $input['amount']
+    'amount' => $input['amount'],
+    'interval' => $input['interval']
 ]);
 
 echo json_encode(['status' => 'success']);
--- php/getRepeatingTransaction.php
+++ php/getRepeatingTransaction.php
@@ -1,7 +1,7 @@
 <?php
 require 'db.php';
 
-$sql = "SELECT * FROM repeatingtransactions WHERE Amount < 0 ORDER BY ValidFrom ASC";
+$sql = "SELECT * FROM repeatingtransactions WHERE ORDER BY ValidFrom ASC";
 $stmt = $pdo->query($sql);
 
 $fixCosts = $stmt->fetchAll();
